bc4555a1cdc3bb6d5ba3e5d0953f6877b2dc0a55
Paul <paulallanmain@gmail.com>
2025-03-24 14:19:53 +0300

    modifies index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
 // Your code here
+document.addEventListener("DOMContentLoaded", () => {
+    const baseURL = "http://localhost:3000/characters";
+    const characterBar = document.getElementById("character-bar");
+
+    fetch(baseURL)
+        .then(response => response.json())
+        .then(characters => {
+            characters.forEach(character => {
+                const span = document.createElement("span");
+                span.textContent = character.name;
+                span.classList.add("character-name");
+                span.dataset.id = character.id;
+                characterBar.appendChild(span);
+            });
+        })
+        .catch(error => console.error("Error fetching characters:", error));
+
+        
+
+});
